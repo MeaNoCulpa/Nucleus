a489064219b8dbaedebbca83075802257825da85
Walfal <raphael.durandy@isep.fr>
2022-05-27 10:29:18 +0200

    javax -> jakarta

--- src/main/java/com/example/nucleus/controller/RegisterController.java
+++ src/main/java/com/example/nucleus/controller/RegisterController.java
@@ -2,11 +2,11 @@ package com.example.nucleus.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.example.nucleus.model.RegisterBean;
 import com.example.nucleus.model.RegisterDao;
